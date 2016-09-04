8d397cd800ff838720cf1f0b1f417334e8432873
zmey-nosak <zmey-nosak@yandex.ru>
2016-09-04 15:13:48 +0300

    add method4

---
+++ task1/src/main/java/subtask2.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Echetik on 03.09.2016.
+ */
+public class subtask2 {
+    private void method(int arg){
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
